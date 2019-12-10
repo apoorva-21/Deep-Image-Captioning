72f0ed5b057d8ed0b9287d716edd9330e10baae6
agokhale <agokhale@agokhale-16399.pods.agokhale.svc.cluster.local>
2019-12-09 16:55:59 -0800

    changed modelsSkip.py

--- modelsSkip.py
+++ modelsSkip.py
@@ -5,7 +5,7 @@ import torchvision
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-class Encoder(nn.Module):
+class EncoderWithSkip(nn.Module):
     """
     Encoder.
     """
